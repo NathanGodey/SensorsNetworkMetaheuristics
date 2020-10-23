202b3e6e8ff7a5392a953cd85c920087201b3919
Nathan Godey <nathan.godey@eleves.enpc.fr>
2020-10-23 11:22:07 +0200

    TODO fix sparse_vector_creation bug

--- sparse.cpp
+++ sparse.cpp
@@ -110,25 +110,26 @@ bool check_modification(sparse_matrix M_capt, sparse_matrix M_comm, sparse_vecto
 
 bool is_eligible(sparse_vector &vect, int k, sparse_matrix &M_comm, sparse_matrix &M_capt) {
     sparse_matrix M_comm_activated(M_comm.n);
+
     M_comm_activated.fill_as_communication_graph(M_comm, vect);
-    unordered_set<int> visited = {0}, current;
+    M_comm_activated.display();
+    unordered_set<int> visited, current;
     vector<int> queue;
     queue.push_back(0);
-    while (!queue.empty()) {
+    while (queue.size()>0) {
         current = M_comm_activated.mat[*queue.begin()];
         for (auto itr = current.begin(); itr != current.end(); ++itr) {
             if (visited.find(*itr) == visited.end()) {
                 queue.push_back(*itr);
             }
         }
-        visited.insert(*queue.begin());
+        visited.insert(queue[0]);
         queue.erase(queue.begin());
     }
     if (visited.size()<M_comm.n){
         vect.isEligible = false;
         return false;
     }
-
     sparse_matrix M_capt_activated(M_comm.n);
     M_capt_activated.fill_as_captation_graph(M_capt, vect);
     for (int i=1; i<M_capt.n; i++) {
--- sparse.h
+++ sparse.h
@@ -11,7 +11,7 @@ public:
     std::unordered_set<int>* vect;
     bool isEligible;
     sparse_vector(){vect = new std::unordered_set<int>;}
-    sparse_vector(std::unordered_set<int> V){vect = new std::unordered_set<int>(V);}
+    sparse_vector(std::unordered_set<int> &V){vect = new std::unordered_set<int>(V);}
     ~sparse_vector(){delete[] vect;}
     void add_point(int point);
     void delete_point(int point);
@@ -25,6 +25,7 @@ public:
     sparse_matrix(int size);
     sparse_matrix(vector<Target> targets, double R);
     sparse_matrix(const sparse_matrix &M);
+    ~sparse_matrix(){mat.clear();}
     void add_edge(int a, int b);
     void delete_edge(int a, int b);
     void fill_as_communication_graph(sparse_matrix &M_comm, sparse_vector &vect);
