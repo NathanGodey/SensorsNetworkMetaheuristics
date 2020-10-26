f43dcf02e224b1d9fbef82ef5d9ab0665bff5d17
Nathan Godey <nathan.godey@eleves.enpc.fr>
2020-10-26 14:37:23 +0100

    added viz and solved bug

--- parser.cpp
+++ parser.cpp
@@ -16,7 +16,7 @@ Parser::Parser(string instanceFile, vector<Target>& targets){
 	while (getline(inFile, line)) {
 			vector<string> parts;
 			split(parts, line, boost::is_any_of(" "));
-			targets.push_back(Target(stod(parts[3]), stod(parts[4]), stoi(parts[1])==0));
+			targets.push_back(Target(stod(parts[3]), stod(parts[4]), 0));
 	}
 	inFile.close();
 }
